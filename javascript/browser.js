a3509aebca37d3b3079ed012b4d53cfa99bf47e5
Dmitriy Chechetkin <morfeusys@gmail.com>
2014-08-30 22:47:07 -0700

    Browser implementation

--- javascript/browser.js
+++ javascript/browser.js
@@ -1,3 +1,58 @@
-/**
- * Created by morfeusys on 30.08.14.
- */
+function Browser() {
+    var win;
+    var websocket;
+    var connected = false;
+    var id = $.cookie('browser-id');
+
+    if (!id) {
+        id = randomNumbersString(6);
+        $.cookie('browser-id', id, {expires: 365});
+    }
+
+    function randomNumbersString(count) {
+        var result = '';
+        for (var i = 0; i < count; i++) {
+            result += Math.floor(Math.random() * 10);
+        }
+        return result;
+    }
+
+    function connect() {
+        if (win && !win.closed) {
+            win.focus();
+        } else {
+            win = window.open('window.html', 'websocket', 'height=' + screen.height + ',width=' + screen.width + ',resizable=yes,scrollbars=yes,toolbar=no,menubar=no,location=no');
+            if (connected) {
+                win.onload = function() {
+                    win.onOpen(id);
+                };
+            }
+        }
+        if (!connected) {
+            websocket = new WebSocket('ws://api.dusi.mobi:8000/browser?id=' + id);
+            websocket.onmessage = function(evt) {
+                win.location = evt.data;
+            };
+            websocket.onopen = function() {
+                connected = true;
+                win.onOpen(id);
+            };
+            websocket.onclose = function() {
+                connected = false;
+                win.location = 'window.html';
+            };
+            websocket.onerror = function() {
+                win.onError();
+                win.location = 'window.html';
+            };
+        }
+    }
+
+    setInterval(function() {
+        if (connected && win && !win.closed) {
+            websocket.send('');
+        }
+    }, 15000);
+
+    Browser.prototype.connect = connect;
+}
