e325891d99b149942e8073626a6719e53e3cd371
Dmitriy Chechetkin <morfeusys@gmail.com>
2014-08-31 23:33:41 -0700

    All new layout

--- js/browser.js
+++ js/browser.js
@@ -29,7 +29,6 @@ function Browser() {
         if (!connected) {
             websocket = new WebSocket('ws://api.dusi.mobi:8000/browser?id=' + id);
             websocket.onmessage = function(evt) {
-                win.focus();
                 win.location = evt.data;
             };
             websocket.onopen = function() {
